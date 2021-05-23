4612a26ec8949c1445f5c992b3ef966faca56c7c
darshanraju <darshan.raju99@gmail.com>
2021-05-23 11:23:11 +1000

    first commit

---
+++ contracts/Cow.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Cow {
+
+    uint256 public totalSupply;
+
+    constructor () public {
+        totalSupply = 100000;
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var Todo = artifacts.require("./TodoList.sol");
+const SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const Todo = artifacts.require("./TodoList.sol");
+var Cow = artifacts.require("./Cow.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(Todo);
+  // deployer.deploy(Cow);
 };
